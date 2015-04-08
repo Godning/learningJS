e951436ca2316b3abe35af07a555c07145e61aa4
Godning <lizhaoning13@gmail.com>
2015-04-08 20:25:36 +0800

    Update main-spec.js
    update test

--- tdd_template/tdd/spec/main-spec.js
+++ tdd_template/tdd/spec/main-spec.js
@@ -9,7 +9,17 @@ describe('Describe of test', function () {
 		
 
 		var result = fight_player(player_a, player_b);
-		var expectResult = 'A win' ;
+		var expectResult = "fight start!\n"+
+			"A attack B ,A's life:10  B's life:7\n"+
+			"B attack A ,A's life:9  B's life:7\n"+
+			"A attack B ,A's life:9  B's life:5\n"+
+			"B attack A ,A's life:8	 B's life:5\n"+
+			"A attack B ,A's life:8	 B's life:3\n"+
+			"B attack A ,A's life:7	 B's life:3\n"+
+			"A attack B ,A's life:7	 B's life:1\n"+
+			"B attack A ,A's life:6	 B's life:1\n"+
+			"A attack B ,A's life:6	 B's life:-1\n" +
+			"A win" ;
 		
 
         expect(result).toBe(expectResult);
